5c8e1880beffcb2e87565f0d94796a82fbafe672
Jerzy Spendel <jerzy.spendel@skygate.io>
2019-10-04 01:26:54 +0200

    Refactor throttle time operator

--- lib/operators/throttle_time.py
+++ lib/operators/throttle_time.py
@@ -10,20 +10,20 @@ def throttle_time(dt):
     class S(ProxySubject):
         def __init__(self):
             super().__init__()
-            self.accept_value = True
+            self.accepting_values = True
             self.task: typing.Optional[asyncio.Task] = None
 
-        async def accept_again(self):
+        async def start_accepting(self):
             await asyncio.sleep(dt)
             self.task = None
-            self.accept_value = True
+            self.accepting_values = True
 
         async def queue_value(self, value):
-            if self.accept_value:
-                self.accept_value = False
+            if self.accepting_values:
+                self.accepting_values = False
                 return await super().queue_value(value)
 
             if not self.task:
-                self.task = asyncio.ensure_future(self.accept_again())
+                self.task = asyncio.ensure_future(self.start_accepting())
 
     return S()
