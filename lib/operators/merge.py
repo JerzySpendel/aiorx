519c36bc53b9f43c31a8dd4f81d215b3ec92a0cc
Jerzy Spendel <jerzy.spendel@skygate.io>
2019-10-09 18:36:21 +0200

    Add merge operator

--- lib/operators/merge.py
+++ lib/operators/merge.py
@@ -5,7 +5,18 @@ from lib.subject import ProxySubject
 
 
 def merge(*obs: Observable):
+
     class S(ProxySubject):
+        def __init__(self):
+            super().__init__()
+            self.completes_counter = 0
+
+        async def on_completed(self):
+            if self.completes_counter < len(obs):
+                self.completes_counter += 1
+            else:
+                return await super().on_completed()
+
         async def on_subscribe(self, observer: Observer):
             for observable in obs:
                 observable.subscribe(self)
